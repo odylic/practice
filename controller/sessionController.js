4b27529f14e890175dc5b1bb6ed48081c9484a36
Jimmy <odylic.lin@gmail.com>
2021-04-20 16:40:38 -0400

    sessionController

--- controller/sessionController.js
+++ controller/sessionController.js
@@ -1,2 +1,39 @@
 const Session = require('../models/sessionModel');
 const app = require('../server');
+const User = require('../models/userModel');
+const {response} = require('express');
+const sessionController = {};
+
+// needed help to see how if user is already logged in with cookie session
+// verify whether or not the session is still valid
+sessionController.isLoggedIn = (req, res, next) => {
+  // documents in the sessions collection will expire due to the schema expire setting
+  console.log('SESSIONCONTROLLER.ISLOGGEDIN FIRED');
+  // find in sessions the cookies.SSID
+  Session.findOne({cookieId: req.cookies.ssid}, (err, session) => {
+    // database error
+    if (err)
+      return next(
+        `Error in sessionController.isLoggedIn: ${JSON.stringify(err)}`
+      );
+    // no session found
+    if (!session) console.log('NO SESSION FOUND');
+    // session found
+    return next();
+  });
+};
+
+sessionController.startSession = (req, res, next) => {
+  console.log('SESSIONCONTROLLER.STARTSESSION FIRED');
+
+  Session.create({cookieId: res.locals.user.id}, (err, session) => {
+    if (err)
+      return next(
+        `Error in sessionController.startSession: ${JSON.stringify(err)}`
+      );
+  });
+  return next();
+};
+
+module.exports = sessionController;
+``
